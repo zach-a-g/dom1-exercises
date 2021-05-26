51638f971e1adaa121ed758755b0f9a8d913d9dd
Zach Gleeson <zachary.a.gleeson@gmail.com>
2021-05-26 16:19:05 -0500

    First commit

---
+++ js2/scripts.js
@@ -0,0 +1,17 @@
+`use strict`;
+
+// Select the h1 tag and set "Toppings" as the text of the heading.
+const heading = document.querySelector(`#heading`);
+heading.innerText = `Toppings`;
+
+// Select the list item 'burger'. Add a class of 'list-item'.
+const root = document.querySelector(`#root`)
+root.append(burger);
+rootElement.append()
+
+
+const burger = document.querySelector(`#burger`);
+burger.appendChild(`#listItem`);
+
+
+
